2a80c11f991c72a43298462fb72794bf9d20eb1b
Navya <newatian@gmail.com>
2021-11-17 13:52:26 +0530

    Ignore update

--- src/impl/kernel/main.c
+++ src/impl/kernel/main.c
@@ -4,10 +4,8 @@
 
 void kernel_main() {
     init();
-    print_char('a');
-    /*asm("hlt");
     print_clear();
     print_set_color(PRINT_COLOR_YELLOW, PRINT_COLOR_BLACK);
     char * buf2 = hexToString(0x1337);
-    write(buf2);*/
+    write(buf2);
 }
