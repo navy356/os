243bfb47dbde7e65e07c030080a8214ce23d38ec
Navya <newatian@gmail.com>
2021-12-04 13:02:38 +0530

    after vid

--- src/impl/kernel/main.c
+++ src/impl/kernel/main.c
@@ -16,7 +16,7 @@ struct Char
 void kernel_main()
 {
     struct multiboot_tag *tag;
-    void * module;
+    void *module;
     if (magic != MULTIBOOT2_BOOTLOADER_MAGIC)
     {
         asm("hlt");
@@ -25,15 +25,26 @@ void kernel_main()
     print_set_color(PRINT_COLOR_YELLOW, PRINT_COLOR_BLACK);
     move_cursor(0, 0);
     print_clear();
-    unsigned long addr = (unsigned long)multiboot_ptr+KERNEL_OFFSET;
+    unsigned long addr = (unsigned long)multiboot_ptr + KERNEL_OFFSET;
     if (addr & 7)
     {
         asm("hlt");
     }
 
-    module=findModule();
+    module = findModule();
+
+    /*int a = malloc(0x8, 0);
+    int b = malloc(0x8, 1);
+    write(hexToString(a));
+    write("\n");
+    write(hexToString(b));
+    write("\n");
+    free(a);
+    int c = malloc(0x8, 0);
+    write(hexToString(c));
+    write("\n");*/
     write(hexToString(module));
     callModule(module);
-    
+
     //startTerminal();
 }
